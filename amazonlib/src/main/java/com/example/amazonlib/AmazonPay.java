485344951faadab2247aa13affe4223ef4c6d2ca
Jilesh Lakhani <jileshl@ridlr.in>
2019-05-14 13:31:38 +0530

    Updating Build gradle and removing explicit dependency

--- amazonlib/src/main/java/com/example/amazonlib/AmazonPay.java
+++ amazonlib/src/main/java/com/example/amazonlib/AmazonPay.java
@@ -1,7 +1,11 @@
 package com.example.amazonlib;
 
+import android.content.Context;
+
 public class AmazonPay {
-    AmazonPay() {
+    AmazonPay(Context context) {
+        amazonpay.silentpay.AmazonPay.getAuthorizationIntent(context);
         System.out.println("Hello World!");
     }
+
 }
